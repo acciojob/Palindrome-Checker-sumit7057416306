193cd550ee74ee60dcd0b0c83ffe41b87cd2eefd
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-01-19 22:47:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-// complete the given function
-
 function palindrome(str){
-
+    let s = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return s === s.split("").reverse().join("");
 }
+console.log(palindrome("racecar")); // Output: true
+console.log(palindrome("RaceCar")); // Output: true
+console.log(palindrome("raceCAR")); // Output: true
+console.log(palindrome("hello")); // Output: false
+
 module.exports = palindrome
